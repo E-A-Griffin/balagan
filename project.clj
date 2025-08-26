519e5e1b1ea5cdc41afe5b91169c4bde08d26dee
Emma Griffin <emma.audrey.g@gmail.com>
2025-08-25 20:45:17 -0600

    cljx->cljc

--- project.clj
+++ project.clj
@@ -4,37 +4,23 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :jar-exclusions [#"\.cljx|\.swp|\.swo|\.DS_Store"]
-  :source-paths ["src/cljx" "target/classes"]
-  :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :master {:dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]]}
+  :jar-exclusions [#"\.cljc|\.swp|\.swo|\.DS_Store"]
+  :source-paths ["src/cljc" "target/classes"]
+  :profiles {:master {:dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]]}
              :dev {:dependencies [[org.clojure/clojurescript "0.0-2138"]]
                    :plugins [[codox "0.8.10"]
-                             [com.keminglabs/cljx "0.3.2"]
                              [lein-cljsbuild "1.0.2"]
                              [com.cemerick/clojurescript.test "0.2.1"]]
-                   :cljx {:builds [{:source-paths ["src/cljx"]
-                                    :output-path "target/classes"
-                                    :rules :clj}
-                                   {:source-paths ["src/cljx"]
-                                    :output-path "target/classes"
-                                    :rules :cljs}
-                                   {:source-paths ["test/cljx"]
-                                    :output-path "target/test-classes"
-                                    :rules :clj}
-                                   {:source-paths ["test/cljx"]
-                                    :output-path "target/test-classes"
-                                    :rules :cljs}]}
                    :cljsbuild {:test-commands {"phantom" ["phantomjs" :runner "target/testable.js"]}
                                :builds [{:source-paths ["target/classes" "target/test-classes"]
                                          :compiler {:output-to "target/testable.js"
                                                     :libs [""]
                                                     :source-map "target/testable.js.map"
                                                     :optimizations :advanced}}]}
-                   :codox {:sources ["src/cljx"]
+                   :codox {:sources ["src/cljc"]
                            :output-dir "doc/api"}}}
-  :aliases  {"all" ["with-profile" "+dev:+master:+1.6"]}
-  :prep-tasks [["cljx" "once"] "javac" "compile"]
+  :aliases  {"all" ["with-profile" "+dev:+master"]}
+  :prep-tasks [["cljc" "once"] "javac" "compile"]
   :repositories {"sonatype" {:url "https://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail :update :always}}
